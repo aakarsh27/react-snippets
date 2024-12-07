59711c7cb05d2691a1fe2db1eabac75b63f67c6a
Aakarsh Sharma <aakarshjai@gmail.com>
2024-12-08 00:59:05 +0530

    now the button above can also update count value

--- usecontext/src/components/Button.jsx
+++ usecontext/src/components/Button.jsx
@@ -1,9 +1,14 @@
+import { useContext } from "react";
 import Component1 from "./Component1"
+import { counterContext } from '../context/context';
 
 const Button = () => {
+
+    const value = useContext(counterContext);
+
   return (
     <>
-    <button><span><Component1/></span> I am a button </button>
+    <button onClick={() => value.setCount((count) => count + 1)}><span><Component1/></span> I am a button </button>
     
     </>
   )
