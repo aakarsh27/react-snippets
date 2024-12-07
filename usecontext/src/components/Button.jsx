a50ade73fb67f46668f9cfcbdfea1214bdd7f0f1
Aakarsh Sharma <aakarshjai@gmail.com>
2024-12-08 00:46:01 +0530

    created components

---
+++ usecontext/src/components/Button.jsx
@@ -0,0 +1,12 @@
+import Component1 from "./Component1"
+
+const Button = () => {
+  return (
+    <>
+    <button><span><Component1/></span> I am a button </button>
+    
+    </>
+  )
+}
+
+export default Button
